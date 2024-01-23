57ec7dcd016c04b6eb8af453c4acda74a843dd48
haoningng <nick.nghn@gmail.com>
2024-01-24 08:09:25 +1100

    fix: print arguments with s if plural

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,7 +4,10 @@ if __name__ == "__main__":
     if len(argv) == 1:
         print("0 arguments.")
     else:
-        print("{} argument:".format(len(argv) - 1))
+        if len(argv) == 2:
+            print("{} argument:".format(1))
+        else:
+            print("{} arguments:".format(len(argv) - 1))
         for index in range(1, len(argv)):
             print("{}: {}".format(index, argv[index]))
             index = index + 1

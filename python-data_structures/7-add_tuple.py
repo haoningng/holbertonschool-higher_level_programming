fce5f1e4ddceb87340a8bcbdcecca698780d3e6f
haoningng <nick.nghn@gmail.com>
2024-01-24 15:58:59 +1100

    style: remove black space and add space after keyword

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -7,5 +7,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
         list_a = list_a + [0] * (2 - len(list_a))
     if len(list_b) < 2:
         list_b = list_b + [0] * (2 - len(list_b))
-    
-    return(((list_a[0] + list_b[0]), (list_a[1] + list_b[1])))
+
+    return ((list_a[0] + list_b[0]), (list_a[1] + list_b[1]))

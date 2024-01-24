3b013786dd9c849263efa6525463d8acda0decae
haoningng <nick.nghn@gmail.com>
2024-01-24 15:32:35 +1100

    fix: print new line if matrix is empty

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for ele in matrix:
-        for integer in ele:
-            if (integer != ele[len(ele) - 1]):
-                print("{:d}".format(integer), end=" ")
-            else:
-                print("{:d}".format(integer))
-    if len(matrix) == 0:
-        print()
+    if matrix == [[]]:
+        print("")
+    else:
+        for ele in matrix:
+            for integer in ele:
+                if (integer != ele[len(ele) - 1]):
+                    print("{:d}".format(integer), end=" ")
+                else:
+                    print("{:d}".format(integer))

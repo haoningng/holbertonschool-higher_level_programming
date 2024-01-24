4e81c2a4f487b0575e84092412a942de88d01375
haoningng <nick.nghn@gmail.com>
2024-01-24 15:26:44 +1100

    fix: use str.format to print integer

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for ele in matrix:
         for integer in ele:
             if (integer != ele[len(ele) - 1]):
-                print("{}".format(integer), end=", ")
+                print("{:d}".format(integer), end=", ")
             else:
-                print("{}".format(integer))
+                print("{:d}".format(integer))

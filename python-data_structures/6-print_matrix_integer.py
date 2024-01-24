3688933dfc8ed518477bb2f5a63bafb66739fe44
haoningng <nick.nghn@gmail.com>
2024-01-24 15:28:38 +1100

    fix: remove comma in between integers

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for ele in matrix:
         for integer in ele:
             if (integer != ele[len(ele) - 1]):
-                print("{:d}".format(integer), end=", ")
+                print("{:d}".format(integer), end=" ")
             else:
                 print("{:d}".format(integer))

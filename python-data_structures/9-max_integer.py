d02018364bca5794024c0bbc4bc9bf262acf7af2
haoningng <nick.nghn@gmail.com>
2024-01-24 16:13:01 +1100

    fix: set max to be first element initially

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if my_list == []:
         return None
     else:
-        max = 0
+        max = my_list[0]
         for i in my_list:
             if i > max:
                 max = i

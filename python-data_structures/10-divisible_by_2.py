57275e2686bd4197e20916408d1489858090b2a3
haoningng <nick.nghn@gmail.com>
2024-01-24 16:29:46 +1100

    feat: add task10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = my_list
+    new_list = my_list.copy()
     idx = 0
     while idx < len(my_list):
         if my_list[idx] % 2 == 0:

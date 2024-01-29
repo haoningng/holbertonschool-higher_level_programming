c68ed91b2a91b795bfdfee3ac3ad7675971f0b5d
haoningng <nick.nghn@gmail.com>
2024-01-29 16:29:44 +1100

    fix: use string format

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range(0, x+1):
-            print(my_list[i], end="")
+        for i in range(0, x):
+            print("{}".format(my_list[i]), end="")
         print()
-        return i
+        return i + 1
     except IndexError:
         print()
         return i

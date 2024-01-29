e858333420c64a6fb54caae5010aa1f8340ee412
haoningng <nick.nghn@gmail.com>
2024-01-29 16:24:44 +1100

    fix: off by one error

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range(0, x):
+        for i in range(1, x+1):
             print(my_list[i], end="")
         print()
-        return i + 1
+        return i
     except IndexError:
         print()
         return i

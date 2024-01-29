80375499e642c6d6d1eae52d6723c966a6bf7229
haoningng <nick.nghn@gmail.com>
2024-01-29 16:21:17 +1100

    fix: not allowed to use len

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for i in range (0, x):
+        for i in range(0, x):
             print(my_list[i], end="")
         print()
-        return x
+        return i
     except IndexError:
         print()
-        return len(my_list)
+        return i

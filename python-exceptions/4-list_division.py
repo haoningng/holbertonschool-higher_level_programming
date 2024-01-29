3d9576c56e41d8cd48afebc58769ca858b17eb8c
haoningng <nick.nghn@gmail.com>
2024-01-29 17:45:39 +1100

    fix: append 0 to list if IndexError

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -11,6 +11,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = 0
             print("division by 0")
         except IndexError:
+            result = 0
             print("out of range")
         finally:
             new_list.append(result)

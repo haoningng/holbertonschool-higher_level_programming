bc99f5790a86ddc318ad08ef755624366c14ddd2
haoningng <nick.nghn@gmail.com>
2024-01-31 08:55:29 +1100

    style: remove comments

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -17,11 +17,6 @@ class Square:
         Args:
             size (int): size of the square
         """
-        # if not (isinstance(size, int)):
-        #     raise TypeError("size must be an integer")
-        # elif size < 0:
-        #     raise ValueError("size must be >= 0")
-        # else:
         self.__size = size
 
     @property

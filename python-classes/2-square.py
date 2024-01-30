4f291f3e373013ce6f9b16fcf4690b6357bb50e5
haoningng <nick.nghn@gmail.com>
2024-01-31 07:01:44 +1100

    feat: add task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,24 @@
+"""This program creates the class Square """
+
+
+class Square:
+    """This defines a square
+
+    This is a simple implementation of a class
+
+    Attributes:
+        None
+
+    """
+    def __init__(self, size=0):
+        """Docstring on the __init__ method.
+
+        Args:
+            size (int): size of the square
+        """
+        if not (isinstance(size, int)):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size

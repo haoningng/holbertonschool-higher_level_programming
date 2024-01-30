e5cc7beb4ff52fa833c9916b73e222b2ab64c3f6
haoningng <nick.nghn@gmail.com>
2024-01-31 06:40:24 +1100

    feat: add task1

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This program creates the class Square """
+
+
+class Square:
+    """This defines a square
+
+    This is a simple implementation of a class
+
+    Attributes:
+        None
+
+    """
+    def __init__(self, size):
+        """Docstring on the __init__ method.
+
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size

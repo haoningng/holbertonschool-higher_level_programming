f30fa2c3d10e631339bb40dd5da256d7c0640383
haoningng <nick.nghn@gmail.com>
2024-02-05 21:59:02 +1100

    docs: add docstrings

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -90,6 +90,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """static method to compare to rectangular areas"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         elif not isinstance(rect_2, Rectangle):

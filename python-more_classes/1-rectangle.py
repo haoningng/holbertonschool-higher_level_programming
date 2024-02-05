1741451b81a9255140ffc7e52d77797358dc58f8
haoningng <nick.nghn@gmail.com>
2024-02-05 17:16:25 +1100

    docs: update docstrings

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -10,15 +10,27 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
+        """Initialise the rectangle
+        
+        Args:
+        width: Width of the rectangle
+        height: Height of the rectangle
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Get the width of the rectangle"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set the width of the rectangle
+        
+        Args
+        value: value of the width
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -27,10 +39,16 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get the height of the rectangle"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Set the height of the rectangle
+        
+        Args:
+        value: value of the height
+        """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:

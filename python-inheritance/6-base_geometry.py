15f50c525f58432948dc0c1f200020c24ce15777
haoningng <nick.nghn@gmail.com>
2024-02-06 22:14:52 +1100

    feat: add tasl6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module creates the BaseGeometry class"""
+
+
+class BaseGeometry:
+    """This creates the BaseGeometry Class
+
+    Attributes:
+    None
+    """
+    def area(self):
+        """Raises an exception with a message"""
+        raise Exception("area() is not implemented")

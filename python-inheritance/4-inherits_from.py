5aa49db5f3d1270d58503b0ddb8843221fb939a4
haoningng <nick.nghn@gmail.com>
2024-02-06 22:08:07 +1100

    fix: revert the last change

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -11,7 +11,7 @@ def inherits_from(obj, a_class):
     a_class: superclass
     """
     if isinstance(obj, a_class):
-        if type(obj) != a_class:
+        if type(obj) is not a_class:
             return True
     else:
         return False

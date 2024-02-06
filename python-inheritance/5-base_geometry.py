068c317a38875410c1471fd97c88b17fbf1fdccd
haoningng <nick.nghn@gmail.com>
2024-02-06 22:12:38 +1100

    docx: added docstrings

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""This module creates the BaseGeometry class"""
+
+
 class BaseGeometry:
+    """This creates the BaseGeometry Class
+
+    Attributes:
+    None
+    """
     pass

81d7458a103c6ce657eadc459d2247711fb61cf7
haoningng <nick.nghn@gmail.com>
2024-02-07 13:15:33 +1100

    docs: add docstrings

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""This modules defines the read_file function"""
+
+
 def read_file(filename=""):
+    """Read a text file and prints it to stdout
+
+    Args:
+    filename: Name of the text tile
+    """
     with open(filename, "r", encoding='UTF-8') as f:
         print(f.read(), end="")

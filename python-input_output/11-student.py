a64f083a0650a34bee826379c4e93938a13e7e92
haoningng <nick.nghn@gmail.com>
2024-02-07 17:50:46 +1100

    feat: add task11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""This module defines the Student Class"""
+
+
+class Student:
+    """This defines the Student Class
+
+    Attributes:
+    None
+    """
+    def __init__(self, first_name, last_name, age):
+        """Instatiation
+
+        Args:
+        first_name: First Name
+        last_name: Last Name
+        age: Age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves a dictionary representation of a Student instance
+
+        Args:
+        attrs: attributes of the student instance
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            new_dict = {}
+            for i in attrs:
+                if hasattr(self, i):
+                    new_dict[i] = getattr(self, i)
+            return new_dict
+
+    def reload_from_json(self, json):
+        """ replaces all attributes of the Student instance
+
+        Args:
+        json: dictionary of new attributes
+        """
+        for key, value in json.items():
+            setattr(self, key, value)

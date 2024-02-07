1bd606417bef18943d1e4e0e7e71c8b5926701cb
haoningng <nick.nghn@gmail.com>
2024-02-07 13:14:14 +1100

    docs: add docstrings

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""This modules defines the write_file function"""
+
+
 def write_file(filename="", text=""):
+    """Writes a string to a text file and return the num of char
+
+    Args:
+    filename: Name of the text file
+    text: Text to be written
+    """
     with open(filename, "r+", encoding='UTF-8') as f:
         f.write(text)
         return len(text)

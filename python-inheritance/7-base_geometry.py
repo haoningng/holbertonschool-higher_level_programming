6159efe1fed3545cbbfc23353467ba62909de4eb
haoningng <nick.nghn@gmail.com>
2024-02-09 09:50:50 +1100

    fix: check if value is bool

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -14,7 +14,9 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """validates value"""
-        if not isinstance(value, int):
+        if isinstance(value, bool):
+            raise TypeError("{} must be an integer".format(name))
+        elif not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))

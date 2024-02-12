8c973567889993596ee5ff219325ad6a3f05867f
haoningng <nick.nghn@gmail.com>
2024-02-12 14:16:16 +1100

    feat: add square class

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""This module creates Rectangle class"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defines the Square Class"""
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Returns string representation of Square"""
+        return ("[Square] ({}) {}/{} - {}/{}"
+                .format(self.id, self.x, self.y, self.width, self.height))

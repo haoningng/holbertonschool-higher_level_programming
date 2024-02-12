f3c8d0fc05db6b36fd4c845961502121dd7a31e5
haoningng <nick.nghn@gmail.com>
2024-02-12 19:47:38 +1100

    feat: add classmethod create()

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -63,3 +63,17 @@ class Base:
             return []
         else:
             return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set
+
+        Args:
+        dictionary: dictionary of attributes
+        """
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        elif cls.__name__ == "Square":
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy

8f5055dad9ce09bf0965d755a2877afc82fac116
haoningng <nick.nghn@gmail.com>
2024-02-12 15:02:44 +1100

    feat: add to_json_string method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module creates first class Base"""
+import json
 
 
 class Base:
@@ -21,3 +22,14 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """Returns the JSON string representation of list_dictionaries
+
+        Args:
+        list_dictionaries(list): list of dictionaries
+        """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)

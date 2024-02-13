f49e2e260c1b5f3bfb5bb1c905b67f7e0686d6b7
haoningng <nick.nghn@gmail.com>
2024-02-13 15:44:26 +1100

    feat: add more test

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -11,9 +11,9 @@ class TestRectangle(unittest.TestCase):
     def test_id(self):
         """Test for id attribute"""
         r1 = Rectangle(10, 2)
-        self.assertEqual(r1.id, 13)
+        self.assertEqual(r1.id, 15)
         r2 = Rectangle(2, 10)
-        self.assertEqual(r2.id, 14)
+        self.assertEqual(r2.id, 16)
         r3 = Rectangle(10, 2, 0, 0, 12)
         self.assertEqual(r3.id, 12)
 
@@ -130,14 +130,21 @@ class TestRectangle(unittest.TestCase):
         """Test for update method - **kwargs"""
         r1 = Rectangle(10, 10, 10, 10)
         r1.update(height=1)
-        self.assertEqual(str(r1), "[Rectangle] (16) 10/10 - 10/1")
+        self.assertEqual(str(r1), "[Rectangle] (18) 10/10 - 10/1")
         r1.update(width=1, x=2)
-        self.assertEqual(str(r1), "[Rectangle] (16) 2/10 - 1/1")
+        self.assertEqual(str(r1), "[Rectangle] (18) 2/10 - 1/1")
         r1.update(y=1, width=2, x=3, id=89)
         self.assertEqual(str(r1), "[Rectangle] (89) 3/1 - 2/1")
         r1.update(x=1, height=2, y=3, width=4)
         self.assertEqual(str(r1), "[Rectangle] (89) 1/3 - 4/2")
 
+    def test_create(self):
+        """Test for create method"""
+        r1 = Rectangle(3, 5, 1)
+        r1_dictionary = r1.to_dictionary()
+        r2 = Rectangle.create(**r1_dictionary)
+        self.assertFalse(r1 is r2)
+        self.assertFalse(r1 == r2)
 
 if __name__ == "__main__":
     unittest.main()

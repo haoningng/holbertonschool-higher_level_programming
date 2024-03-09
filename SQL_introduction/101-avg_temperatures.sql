994429bc22534cdf017c254bf332155e57526b45
haoningng <nick.nghn@gmail.com>
2024-03-09 15:03:50 +1100

    feat: add task 101

---
+++ SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,8 @@
+-- This script displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+USE hbtn_0c_0;
+SOURCE temperatures.sql;
+
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;

57661b489b8539ca9fd7a00c4fa39c06ae2123b8
haoningng <nick.nghn@gmail.com>
2024-03-09 14:06:50 +1100

    fix: change AS clause to before FROM

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,2 +1,3 @@
 -- This script computes the score average of all records in the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT AVG(score) FROM second_table AS average
+SELECT AVG(score) AS average
+FROM second_table 

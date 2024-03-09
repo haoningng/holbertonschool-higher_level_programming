c9b67fdee959528448a98ccdc431cf142e231cb1
haoningng <nick.nghn@gmail.com>
2024-03-09 13:44:36 +1100

    fix: use UNION ALL

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,10 @@
 -- This script creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT)
 
-INSERT INTO second_table (id, name, score) VALUES (1, “John”, 10)
-INSERT INTO second_table (id, name, score) VALUES (2, “Alex”, 3)
-INSERT INTO second_table (id, name, score) VALUES (3, “Bob”, 14)
-INSERT INTO second_table (id, name, score) VALUES (4, “George”, 8)
+SELECT 1 AS id, "John" AS name, 10 AS score
+UNION ALL
+SELECT 2 AS id, "Alex" AS name, 3 AS score
+UNION ALL
+SELECT 3 AS id, "Bob" AS name, 14 AS score
+UNION ALL
+SELECT 4 AS id, "George" AS name, 8 AS score

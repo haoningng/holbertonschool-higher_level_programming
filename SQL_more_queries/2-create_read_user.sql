35f7a7f8350863d8de5b5e018aa178fe92d19b43
haoningng <nick.nghn@gmail.com>
2024-03-12 10:59:10 +1100

    fix: grant usage privilege

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -4,5 +4,5 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'hbtn_0d_2'@'localhost';
 
 SET PASSWORD FOR 'hbtn_0d_2'@'localhost' = 'user_0d_2_pwd';
-
+GRANT USAGE ON *.* TO `user_0d_2`@`localhost`  
 GRANT SELECT ON hbtn_0d_2.* TO 'hbtn_0d_2'@'localhost';

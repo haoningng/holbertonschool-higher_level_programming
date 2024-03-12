f5c834655b373499613fbb59ab9bf93333eacbe6
haoningng <nick.nghn@gmail.com>
2024-03-12 15:58:28 +1100

    fix: change keyword to UPPERCASE

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -2,7 +2,7 @@
 SELECT name FROM tv_genres AS a
 RIGHT JOIN tv_show_genres AS b
 ON a.id = b.genre_id
-RIGHT JOIN tv_shows as c
+RIGHT JOIN tv_shows AS c
 ON b.show_id = c.id
 WHERE c.title = 'DEXTER'
 ORDER BY name ASC;

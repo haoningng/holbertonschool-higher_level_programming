05c965e9f2d0e7ce2e95b5f78bf9ce14ec19d309
haoningng <nick.nghn@gmail.com>
2024-03-12 16:06:16 +1100

    fix: change to CROSS JOIN

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- This script lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
 SELECT c.title AS title, a.name AS name FROM tv_genres AS a
-JOIN tv_show_genres AS b
+CROSS JOIN tv_show_genres AS b
 ON a.id = b.genre_id
 JOIN tv_shows AS c
 ON b.show_id = c.id

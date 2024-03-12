713a90ca05b368ef2fcd9e2e313d5816e66a859a
haoningng <nick.nghn@gmail.com>
2024-03-12 11:21:44 +1100

    fix: typo in user name

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,8 +1,8 @@
 -- This script creates the database hbtn_0d_2 and the user user_0d_2.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 
-CREATE USER IF NOT EXISTS 'hbtn_0d_2'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
 
-SET PASSWORD FOR 'hbtn_0d_2'@'localhost' = 'user_0d_2_pwd';
+SET PASSWORD FOR 'user_0d_2'@'localhost' = 'user_0d_2_pwd';
 GRANT USAGE ON *.* TO `user_0d_2`@`localhost`  
-GRANT SELECT ON hbtn_0d_2.* TO 'hbtn_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';

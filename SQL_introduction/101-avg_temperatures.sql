91068bbf775bba714f121186f6b28ce59137befd
haoningng <nick.nghn@gmail.com>
2024-03-12 16:09:24 +1100

    fix: remove the SOUCE clause

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,5 @@
 -- This script displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
 USE hbtn_0c_0;
-SOURCE temperatures.sql;
 
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures

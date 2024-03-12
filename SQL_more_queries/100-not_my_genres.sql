f359ca1183c8666d9c104784aea249fe7e17104d
haoningng <nick.nghn@gmail.com>
2024-03-12 16:29:24 +1100

    fix: USE additional SELECT clause

--- SQL_more_queries/100-not_my_genres.sql
+++ SQL_more_queries/100-not_my_genres.sql
@@ -1,9 +1,12 @@
 -- This script uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
-SELECT a.name AS name FROM tv_genres AS a
-RIGHT JOIN tv_show_genres AS b
-ON a.id = b.genre_id
-RIGHT JOIN tv_shows AS c
-ON b.show_id = c.id
-WHERE NOT c.title = 'DEXTER'
-GROUP BY name
-ORDER BY name ASC;
+SELECT name FROM tv_genres 
+WHERE name NOT IN (
+    SELECT a.name AS name FROM tv_genres AS a
+    RIGHT JOIN tv_show_genres AS b
+    ON a.id = b.genre_id
+    RIGHT JOIN tv_shows AS c
+    ON b.show_id = c.id
+    WHERE c.title = 'DEXTER'
+    ORDER BY name ASC
+);
+

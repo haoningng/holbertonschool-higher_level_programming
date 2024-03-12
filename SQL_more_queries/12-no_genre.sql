7def0a25f298494b321bfa99d53be92f2944ebdc
haoningng <nick.nghn@gmail.com>
2024-03-12 15:40:06 +1100

    fix: change to where b.show_id is NULL

--- SQL_more_queries/12-no_genre.sql
+++ SQL_more_queries/12-no_genre.sql
@@ -1,5 +1,6 @@
 -- This script lists all shows contained in hbtn_0d_tvshows without a genre linked
 SELECT a.title, b.genre_id FROM tv_shows AS a
-CROSS JOIN tv_show_genres AS b
+LEFT JOIN tv_show_genres AS b
 ON a.id = b.show_id
-WHERE b.genre_id IS NULL;
+WHERE b.show_id IS NULL
+ORDER BY a.title, b.genre_id ASC;

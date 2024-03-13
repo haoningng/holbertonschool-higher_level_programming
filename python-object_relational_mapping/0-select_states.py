78fda2a897c267881e0286bfd54902cef13aaaf3
haoningng <nick.nghn@gmail.com>
2024-03-14 10:02:12 +1100

    feat: add task0

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This script lists all states from the database hbtn_0e_0_usa"""
+import MySQLdb
+
+db = MySQLdb.connect(host="localhost", port="3306", user="root", passwd="root", db="hbtn_0e_0_usa")
+cur = db.cursor()
+cur.execute("SELECT id, name FROM states")
+rows = cur.fetchall()
+for row in rows:
+    print("({}, '{}')".format(row[0] + 1, row[1]))

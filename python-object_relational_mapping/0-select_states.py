5d94e732285f3da9e3d4efe6b8d0d05f8ccfe818
haoningng <nick.nghn@gmail.com>
2024-03-14 10:04:44 +1100

    fix: change port number form str to int

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,7 +2,7 @@
 """This script lists all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 
-db = MySQLdb.connect(host="localhost", port="3306", user="root", passwd="root", db="hbtn_0e_0_usa")
+db = MySQLdb.connect(host="localhost", port=3306, user="root", passwd="root", db="hbtn_0e_0_usa")
 cur = db.cursor()
 cur.execute("SELECT id, name FROM states")
 rows = cur.fetchall()

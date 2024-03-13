da57c1d4f72484530449a53e6f63527e31325edb
haoningng <nick.nghn@gmail.com>
2024-03-14 10:35:23 +1100

    fix: add __name__=__main__ also more: add ORDER BY clause close the cursor and db print every row

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,12 +4,13 @@ import sys
 import MySQLdb
 
 
-db = MySQLdb.connect(user=sys.argv[1],
-                     passwd=sys.argv[2], db=sys.argv[3])
-cur = db.cursor()
-cur.execute("SELECT id, name FROM states")
-rows = cur.fetchall()
-for row in rows:
-    print("({}, '{}')".format(row[0] + 1, row[1]))
-cur.close()
-db.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1],
+                        passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()

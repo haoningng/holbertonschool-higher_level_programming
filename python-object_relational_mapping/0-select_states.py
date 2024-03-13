351445ecbae356bb4f19347ac23d8c4e62da23b3
haoningng <nick.nghn@gmail.com>
2024-03-14 10:12:30 +1100

    fix: use sys.argv to fetch the arguments

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """This script lists all states from the database hbtn_0e_0_usa"""
+import sys
 import MySQLdb
 
 
-db = MySQLdb.connect(host="localhost", port=3306, user="root",
-                     passwd="root", db="hbtn_0e_0_usa")
+db = MySQLdb.connect(user=sys.argv[1],
+                     passwd=sys.argv[2], db=sys.argv[3])
 cur = db.cursor()
 cur.execute("SELECT id, name FROM states")
 rows = cur.fetchall()
 for row in rows:
     print("({}, '{}')".format(row[0] + 1, row[1]))
+cur.close()
+db.close()

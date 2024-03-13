43e513090845c173b7d27ed07355cf5b038212d1
haoningng <nick.nghn@gmail.com>
2024-03-14 10:52:40 +1100

    fix: check first letter of state in the for loop

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This script lists all states from the database hbtn_0e_0_usa"""
+"""This script lists all states with a name starting with N (upper N)"""
 import sys
 import MySQLdb
 
@@ -8,10 +8,10 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("""SELECT id, name FROM states WHERE
-                name like 'N%' ORDER BY id ASC""")
+    cur.execute("SELECT id, name FROM states ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
-        print(row)
+        if row[1][0] == 'N':
+            print(row)
     cur.close()
     db.close()

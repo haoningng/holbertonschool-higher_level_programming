fe15cd24577e018abae5b481fab4e629cf57b053
haoningng <nick.nghn@gmail.com>
2024-03-14 15:05:24 +1100

    fix: print the first instance

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for instance in session.query(State).order_by(State.id).first():
-        print("{}: {}".format(instance.id, instance.name))
+    instance = session.query(State).order_by(State.id).first()
+    print("{}: {}".format(instance.id, instance.name))

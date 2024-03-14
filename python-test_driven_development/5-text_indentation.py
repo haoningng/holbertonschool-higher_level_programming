60941eeb2ebd3f362bdadfb7b7bf80c08e7b1200
haoningng <nick.nghn@gmail.com>
2024-03-15 09:26:40 +1100

    style: remove whitespace

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -15,7 +15,7 @@ def text_indentation(text):
     """
     if not (isinstance(text, str)):
         raise TypeError("text must be a string")
-    
+
     start_index = 0
     for i in range(len(text)):
         if text[i] in (".", "?", ":"):

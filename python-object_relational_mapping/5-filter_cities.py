7ef7dbf851938c4007d5a338755ad0d18229d60f
haoningng <nick.nghn@gmail.com>
2024-03-14 12:31:43 +1100

    style: line exceeded 79 char

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""This script lists all cities of that state, using the database hbtn_0e_4_usa"""
+"""This script lists all cities of that state,
+using the database hbtn_0e_4_usa"""
 import sys
 import MySQLdb
 

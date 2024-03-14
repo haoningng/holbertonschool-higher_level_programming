5d574594804af0a234c768c5a51f503cb1aa4506
haoningng <nick.nghn@gmail.com>
2024-03-14 11:52:24 +1100

    fix: change id to c.id

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    query = """SELECT id, c.name, s.name FROM states AS s
+    query = """SELECT c.id, c.name, s.name FROM states AS s
                 JOIN cities AS c
                 ON c.state_id = s.id
                 ORDER BY c.id ASC"""

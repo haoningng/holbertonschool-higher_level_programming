59a9f10a666630f8eb65cf6fe07dcee9a20a96fa
haoningng <nick.nghn@gmail.com>
2024-03-14 15:36:15 +1100

    feat: add task 10

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""prints the State object with the name passed as argument
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    instance = session.query(State).filter(State.name == "%s", (sys.argv[4], )).first()
+    if instance is None:
+        print("Not found")
+    else:
+        print("{}".format(instance.id))

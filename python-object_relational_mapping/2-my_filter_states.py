c45cf9bbb7c15f8e54edca8e6cb2aeb7c44dd4fe
haoningng <nick.nghn@gmail.com>
2024-03-14 11:07:50 +1100

    style: keep line less than 79 char

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("""SELECT id, name FROM states
-                WHERE BINARY name = '{}' ORDER BY id ASC""".format(sys.argv[4]))
+                WHERE BINARY name = '{}' ORDER BY id ASC"""
+                .format(sys.argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
